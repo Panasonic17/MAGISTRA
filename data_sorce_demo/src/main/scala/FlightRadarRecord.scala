16e69a00cbc586689bbec13cad8fa476c9be185f
Oleksandr_Shainoga <Oleksandr_Shainoga@epam.com>
2018-09-18 17:50:43 +0300

    MIDDLE DEVELOPER MAGISTR GIT COMMENT

--- data_sorce_demo/src/main/scala/FlightRadarRecord.scala
+++ data_sorce_demo/src/main/scala/FlightRadarRecord.scala
@@ -7,6 +7,15 @@ case class FlightRadarRecord(
                               CALIBRATED_ALTITUDE: String,
                               GROUND_SPEED: String,
                               _8: String,
-                              RADAR: String, _10: String, _11: String, _12: String, _13: String, _14: String, _15: String, _16: String, _17: String, _18: String, _19: String, _20: String)
+                              RADAR: String,
+                              TYPE: String,
+                              REGISTRATION: String,
+                              timestamp: String,
+                              from_City: String,
+                              to_City: String,
+                              MB_PLAIN_NAME: String,
+                              _16: String,
+                              _17: String,
+                              _18: String, _19: String, _20: String)
 
 //  input ,"1de85770":["4CA621",46.3482,-1.8333,224,37000,415,"2125","T-LFBH1","B738","EI-DWT",1537276192,"EIN","AGA","FR3846",0,0,"RYR3846",0,"RYR"]
